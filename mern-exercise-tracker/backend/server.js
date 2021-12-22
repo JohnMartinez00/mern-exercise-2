8b4dd16018931e01b79506f36d11fcb3d14974e6
john98989898 <96251170+john98989898@users.noreply.github.com>
2021-12-23 00:25:13 +0800

    Add files via upload

---
+++ mern-exercise-tracker/backend/server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.ATLAS_URI;
+mongoose
+  .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(port, () =>
+      console.log("MongoDB database connection established successfully")
+    )
+  )
+  .catch((error) => console.log(error.message));
+
+const exercisesRouter = require("./routes/exercises");
+const usersRouter = require("./routes/users");
+
+app.use("/exercises", exercisesRouter);
+app.use("/users", usersRouter);
+
+app.post("/", function(req, res) {
+  res.send("POST request to the homepage");
+});
